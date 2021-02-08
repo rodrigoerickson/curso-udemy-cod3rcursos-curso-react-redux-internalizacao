4872dbd0a4b9077d3d2807a552245ab8e8cafd6f
Rodrigo erickson <rodrigo.erickson@gmail.com>
2021-02-08 08:50:53 -0300

    alterando action para trabalar com constantes

--- src/store/actions/numeros.js
+++ src/store/actions/numeros.js
@@ -1,13 +1,20 @@
-export function alterarNumeroMinimo(novoNumero){
+import {
+    NUM_MIN_ALTERADO,
+    NUM_MAX_ALTERADO
+} from './actionTypes'
+
+// Action Creator
+export function alterarNumeroMinimo(novoNumero) {
     return {
-        type:'NUM_MIN_ALTERADO',
-        payload:novoNumero
+        type: NUM_MIN_ALTERADO,
+        payload: novoNumero
     }
 }
 
-export function alterarNumeroMaximo(novoNumero){
+// Action Creator
+export function alterarNumeroMaximo(novoNumero) {
     return {
-        type:'NUM_MAX_ALTERADO',
-        payload:novoNumero
+        type: NUM_MAX_ALTERADO,
+        payload: novoNumero
     }
 }
